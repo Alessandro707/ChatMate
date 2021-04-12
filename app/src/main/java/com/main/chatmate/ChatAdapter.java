d4c9d5d6d5780891a03f22f4aa2e35cec25e4f9f
Alessandro707 <boschi.03@gmail.com>
2021-04-12 23:29:09 +0200

    activities

---
+++ app/src/main/java/com/main/chatmate/ChatAdapter.java
@@ -0,0 +1,39 @@
+package com.main.chatmate;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ChatAdapter extends BaseAdapter {
+	ArrayList<Chat> chats;
+	
+	@Override
+	public int getCount() {
+		return chats.size();
+	}
+	
+	@Override
+	public Object getItem(int position) {
+		if(position >= chats.size()) return null;
+		return chats.get(position);
+	}
+	
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if(convertView==null){
+			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+			convertView = inflater.inflate(R.layout.chat_layout, parent, false);
+		}
+		return convertView;
+	}
+}
