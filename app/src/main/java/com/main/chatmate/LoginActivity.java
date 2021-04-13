7e67352ab732764c40071028af51a4b8aede8af3
Alessandro707 <boschi.03@gmail.com>
2021-04-14 01:08:32 +0200

    auth

--- app/src/main/java/com/main/chatmate/LoginActivity.java
+++ app/src/main/java/com/main/chatmate/LoginActivity.java
@@ -28,14 +28,19 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
+	private boolean codeSent = false;
+	private String mVerificationId;
+	private PhoneAuthProvider.ForceResendingToken mResendToken;
 	private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 		@Override
 		public void onVerificationCompleted(PhoneAuthCredential credential) {
-			signInWithPhoneAuthCredential(credential);
+			System.out.println("VERIFICATION COMPLETED");
+			signInWithPhoneAuthCredential(credential); // credenziali inserite corrette, accesso
 		}
 		
 		@Override
 		public void onVerificationFailed(FirebaseException e) {
+			System.out.println("VERIFICATION FAILED");
 			if (e instanceof FirebaseAuthInvalidCredentialsException) {
 				// Invalid request
 			} else if (e instanceof FirebaseTooManyRequestsException) {
@@ -45,21 +50,23 @@ public class LoginActivity extends AppCompatActivity {
 		
 		@Override
 		public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
-			String mVerificationId = verificationId;
-			PhoneAuthProvider.ForceResendingToken mResendToken = token;
+			mVerificationId = verificationId;
+			mResendToken = token;
+			System.out.println("CODE SENT");
+			codeSent = true;
 		}
 	};
-	
 	
 	@Override
 	protected void onStart() {
 		super.onStart();
 		
-		FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+		FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); // utente già loggato, easy
 		if (user != null) {
 			Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 			startActivity(intent);
 		}
+		// utente non loggato, oh shiet
 	}
 	
 	@Override
@@ -67,37 +74,46 @@ public class LoginActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
-		EditText numero = findViewById(R.id.login_text_phonenumber);
+		EditText numero = findViewById(R.id.login_text_phone_number);
 		Button login = findViewById(R.id.login_button);
+		EditText code_field = findViewById(R.id.login_verification_code);
 		
+		// FORMATI SUPPORTATI CHE HO SCOPERTO:
+		//+1 650-555-3434
+		//+393479978847
 		login.setOnClickListener(v -> {
-			// FORMATI SUPPORTATI CHE HO SCOPERTO:
-			//+1 650-555-3434
-			//+393479978847
-			PhoneAuthOptions options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
-					.setPhoneNumber(numero.getText().toString())
-					.setTimeout(10L, TimeUnit.MINUTES)
-					.setActivity(this).setCallbacks(mCallbacks)
-					.build();
-			
-			PhoneAuthProvider.verifyPhoneNumber(options);
+			if(!codeSent && !numero.getText().toString().isEmpty()) {
+				PhoneAuthOptions options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+						.setPhoneNumber(numero.getText().toString())
+						.setTimeout(120L, TimeUnit.SECONDS) // tempo dopo il quale il codice di verifica scade
+						.setActivity(this).setCallbacks(mCallbacks)
+						.build();
+				
+				PhoneAuthProvider.verifyPhoneNumber(options);
+			}
+			else{
+				codeSent = false;
+				String code = code_field.getText().toString();
+				if(!code.isEmpty() && code.length() >= 6){
+					PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+					signInWithPhoneAuthCredential(credential);
+				}
+			}
 		});
 	}
 	
 	private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-		FirebaseAuth.getInstance().signInWithCredential(credential)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task) {
-						if (task.isSuccessful()) {
-							FirebaseUser user = Objects.requireNonNull(task.getResult()).getUser();
-						} else {
-							if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-								// The verification code entered was invalid
-							}
-						}
-					}
-				});
+		FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this, task -> {
+			if (task.isSuccessful()) {
+				System.out.println("TASK SUCCESSATA");
+				// L'utente esiste yeee
+			} else {
+				if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+					// The verification code entered was invalid
+					System.out.println("TASK FALLATA");
+				}
+			}
+		});
 	}
 	
 }
